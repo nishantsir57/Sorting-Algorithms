73cd0c905f66a0888ade3338df2c88db5bb35986
nishantsir57 <38082502+nishantsir57@users.noreply.github.com>
2019-04-03 11:31:51 +0530

    InsertionSort.java

---
+++ InsertionSort.java
@@ -0,0 +1,15 @@
+static void insertionSort(int arr[])
+    {
+        int length=arr.length;
+        for(int i=1;i<length;i++)
+        {
+            int key=arr[i];
+            int j=i-1;
+            while(j >= 0 && arr[j] > key)
+            {
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=key;
+        }
+    }
